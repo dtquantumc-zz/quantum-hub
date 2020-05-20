85169d030c97aa2bcbb8009b3c60655fc6baa18f
Tanraj <tsaran70@gmail.com>
2020-05-20 14:39:22 -0700

    [Closes #22] Move App.css Styling to app.js
    What: Move App.css styling to app.js
    Why: This is the convention that Material Kit React uses (refer to all the style files under the quantum-hub > src > assets folder)

--- quantum-hub/src/assets/jss/material-kit-react/views/app.js
+++ quantum-hub/src/assets/jss/material-kit-react/views/app.js
@@ -31,6 +31,19 @@ const appStyle = {
     borderRadius: '6px',
     boxShadow:
       '0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)'
+  },
+  gridContainer: {
+    justifyContent: 'center',
+    '& > :not(.game)': {
+      marginTop: '8px'
+    }
+  },
+  nurseSchedulingInput: {
+    justifyContent: 'center'
+  },
+  sudokuInput: {
+    display: 'flex',
+    justifyContent: 'center'
   }
 }
 
