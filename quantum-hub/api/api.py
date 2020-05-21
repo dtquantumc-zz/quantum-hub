e2448a7b8dc6061765e86cdff056b3e5ea1f3078
Ari Blondal <ari.blondal@gmail.com>
2020-05-21 14:57:19 -0700

    Added engine to packages

--- quantum-hub/api/api.py
+++ quantum-hub/api/api.py
@@ -1,8 +1,12 @@
 import time
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/test_server')
 def test_server():
 	return {'test': 'complete'}
+
+@app.route('/')
+def my_index():
+	return render_template("../build/index.html")
