fce97c434b87f89f53404550c973a32f49f41b9b
Tanraj <tsaran70@gmail.com>
2020-05-26 09:36:01 -0700

    [Closes #48] Add Material Kit React Card Styles
    What: Added Material Kit React style classes for the Card component
    (under the quantum-hub / src / assets / jss / material-kit-react / components folder)

---
+++ quantum-hub/src/assets/jss/material-kit-react/components/cardHeaderStyle.js
@@ -0,0 +1,48 @@
+/*!
+
+ =========================================================
+ * Material Kit React - v1.8.0 based on Material Kit - v2.0.2
+ =========================================================
+
+ * Product Page: https://www.creative-tim.com/product/material-kit-react
+ * Copyright 2019 Creative Tim (https://www.creative-tim.com)
+ * Licensed under MIT (https://github.com/creativetimofficial/material-kit-react/blob/master/LICENSE.md)
+
+ =========================================================
+
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ */
+
+import {
+  warningCardHeader,
+  successCardHeader,
+  dangerCardHeader,
+  infoCardHeader,
+  primaryCardHeader,
+  geeringupCardHeader
+} from '../../material-kit-react.js'
+
+const cardHeaderStyle = {
+  cardHeader: {
+    borderRadius: '3px',
+    padding: '1rem 15px',
+    marginLeft: '15px',
+    marginRight: '15px',
+    marginTop: '-30px',
+    border: '0',
+    marginBottom: '0'
+  },
+  cardHeaderPlain: {
+    marginLeft: '0px',
+    marginRight: '0px'
+  },
+  warningCardHeader,
+  successCardHeader,
+  dangerCardHeader,
+  infoCardHeader,
+  primaryCardHeader,
+  geeringupCardHeader
+}
+
+export default cardHeaderStyle
