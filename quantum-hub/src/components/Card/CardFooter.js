bde0386390580ebd5cc21707c9fc4612420e5fd4
Tanraj <tsaran70@gmail.com>
2020-05-26 11:57:33 -0700

    [Closes #54] Add Material Kit React Card Component
    What: Added Material Kit React classes for the Card component
    (under the quantum-hub / src / components / Card folder)

---
+++ quantum-hub/src/components/Card/CardFooter.js
@@ -0,0 +1,48 @@
+/*!
+
+ =========================================================
+ * Material Kit React - v1.8.0 based on Material Kit - v2.0.2
+ =========================================================
+
+ * Product Page: https://www.creative-tim.com/product/material-kit-react
+ * Copyright 2019 Creative Tim (https://www.creative-tim.com)
+ * Licensed under MIT (https://github.com/creativetimofficial/material-kit-react/blob/master/LICENSE.md)
+
+ =========================================================
+
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ */
+
+import React from 'react'
+// nodejs library that concatenates classes
+import classNames from 'classnames'
+// nodejs library to set properties for components
+import PropTypes from 'prop-types'
+// @material-ui/core components
+import { makeStyles } from '@material-ui/core/styles'
+// @material-ui/icons
+
+// core components
+import styles from '../../assets/jss/material-kit-react/components/cardFooterStyle.js'
+
+const useStyles = makeStyles(styles)
+
+export default function CardFooter (props) {
+  const classes = useStyles()
+  const { className, children, ...rest } = props
+  const cardFooterClasses = classNames({
+    [classes.cardFooter]: true,
+    [className]: className !== undefined
+  })
+  return (
+    <div className={cardFooterClasses} {...rest}>
+      {children}
+    </div>
+  )
+}
+
+CardFooter.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node
+}
