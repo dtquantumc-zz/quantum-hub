9f3862ca6286e419568477eb77039addac4c4c9c
Tanraj <tsaran70@gmail.com>
2020-05-29 09:30:49 -0700

    [Closes #80] Standardize Console Width
    What: Console width changed to be equal to
    Description Card's width
    Why: Reasons in NOTE section of #79

---
+++ quantum-hub/src/assets/jss/material-kit-react/components/consoleStyle.js
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+/*
+ * (C) Copyright 2020
+ * Diversifying Talent in Quantum Computing, Geering Up, UBC
+ */
+
+const consoleStyle = theme => ({
+  console: {
+    width: 'calc(((1140px - (462.047px + 2 * 8px)) / 2) - 2 * 8px)',
+    margin: theme.spacing(1),
+    '& > *': {
+      width: 'calc(314.9765px - (2 * 15px))'
+    }
+  }
+})
+
+export default consoleStyle
