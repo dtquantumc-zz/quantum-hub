287275d7311d3ce7d2cb99b4e3121d9a4f7e8a0c
Tanraj <tsaran70@gmail.com>
2020-05-29 09:26:15 -0700

    [Closes #79] Change Description Card Component
    What: Switch to using Material-Kit-React's
    Card component instead of Material-UI's
    Complex Interaction card.
    Why: The description was too long to fit in the old component.
    Although it did expand, it would extend the shell body so far down
    that the whitespace to its left made the design look incomplete.
    The new component will click to open a Dialog over the app shell
    which the user can close.
    
    NOTE: Description Card width of 314.9765px is chosen because:
    - the Grid Container's max width is 1140px
    - the Sudoku Game's div is 462.047px + 2 * 8px (margin of 8px on each side)
    - this leaves ((1140px - (462.047px + 2 * 8px)) / 2) - 2 * 8px
    (margin of 8px on each side) = 314.9765px equal width for the
    Console and Description Card.

--- quantum-hub/src/components/Card/DescriptionCard.js
+++ quantum-hub/src/components/Card/DescriptionCard.js
@@ -1,116 +1,174 @@
+// SPDX-License-Identifier: MIT
+/*
+ * (C) Copyright 2020
+ * Diversifying Talent in Quantum Computing, Geering Up, UBC
+ */
+
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import clsx from 'clsx'
-import Card from '@material-ui/core/Card'
-import CardHeader from '@material-ui/core/CardHeader'
-// import CardMedia from '@material-ui/core/CardMedia'
-import CardContent from '@material-ui/core/CardContent'
-import CardActions from '@material-ui/core/CardActions'
-import Collapse from '@material-ui/core/Collapse'
-import IconButton from '@material-ui/core/IconButton'
+
+import Card from './Card.js'
+import CardHeader from './CardHeader'
+import CardBody from './CardBody'
+
+import GridContainer from '../Grid/GridContainer.js'
+import GridItem from '../Grid/GridItem'
+
+// import widgetList from '../Widget/Widget.json'
+import Button from '../CustomButtons/Button'
+
 import Typography from '@material-ui/core/Typography'
-import { red } from '@material-ui/core/colors'
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import Link from '@material-ui/core/Link'
 
-import widgetList from '../Widget/widgetList'
+import styles from '../../assets/jss/material-kit-react/components/descriptionCardStyle.js'
+const useStyles = makeStyles(styles)
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: 259,
-    height: 430
-  },
-  media: {
-    height: 0,
-    paddingTop: '56.25%' // 16:9
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest
-    })
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)'
-  },
-  avatar: {
-    backgroundColor: red[500]
-  }
-}))
-
-export default function DescriptionCard (props) {
+export default function DescriptionCard () {
   const classes = useStyles()
-  const [expanded, setExpanded] = React.useState(false)
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded)
-  }
-  let widgetInfo
-  if (widgetList[props.widget]) {
-    widgetInfo = widgetList[props.widget]
-  } else {
-    widgetInfo = {
-      title: 'Title not found',
-      subheader: 'Subheader not found',
-      description: 'Description not found'
+  const [open, setOpen] = React.useState(false)
+  const [scroll, setScroll] = React.useState('paper')
+  const descriptionElementRef = React.useRef(null)
+
+  const [cardAnimaton, setCardAnimation] = React.useState('cardHidden')
+  setTimeout(function () {
+    setCardAnimation('descriptionCard')
+  }, 700)
+
+  React.useEffect(() => {
+    if (open) {
+      const { current: descriptionElement } = descriptionElementRef
+      if (descriptionElement !== null) {
+        descriptionElement.focus()
+      }
     }
+  }, [open])
+
+  const handleClickOpen = (scrollType) => () => {
+    setOpen(true)
+    setScroll(scrollType)
   }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  // let widgetInfo
+  // if (widgetList[props.widget]) {
+  //   widgetInfo = widgetList[props.widget]
+  // } else {
+  //   widgetInfo = {
+  //     title: 'Title not found',
+  //     subheader: 'Subheader not found',
+  //     description: 'Description not found'
+  //   }
+  // }
 
   return (
-    <Card className={classes.root}>
-      <CardHeader
-        title={widgetInfo.title}
-        subheader={widgetInfo.subheader}
-      />
-      {/* <CardMedia
-        className={classes.media}
-        image='/static/images/cards/paella.jpg'
-        title='Paella dish'
-      /> */}
-      <CardContent>
-        <Typography variant='body2' color='textSecondary' component='p'>
-          {widgetInfo.description}
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label='show more'
-        >
-          <ExpandMoreIcon />
-        </IconButton>
-      </CardActions>
-      <Collapse in={expanded} timeout='auto' unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-            minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-            again without stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
-        </CardContent>
-      </Collapse>
-    </Card>
+    <GridContainer className={classes.container} justify='center'>
+      <GridItem>
+        <Card className={classes[cardAnimaton]}>
+          <form className={classes.form}>
+            <CardHeader color='geeringup' className={classes.cardHeader}>
+              How It Works
+            </CardHeader>
+            <CardBody>
+              <h4 className={classes.cardTitle}>The Sudoku Problem</h4>
+              <Typography align='left' paragraph variant='body2' color='textSecondary' component='p'>
+                Sudoku is a logic-based, number-placement puzzle. To complete it, you must fill
+                a 9x9 grid with single digits such that each column, row, and subgrid contain all
+                of the digits from 1 to 9.
+              </Typography>
+              <Typography align='left' paragraph variant='body2' color='textSecondary' component='p'>
+                While this puzzle can be solved on a <i>classical</i> computer, let's try solving
+                it on a <i>quantum annealer</i>. In particular, we will be using D-Wave System's 2000Q
+                Quantum Processing Unit. To use this web application, you require a unique API token from
+                D-Wave Systems. You can obtain one by signing up for a <i>free</i> LEAP account on
+                their <Link href='https://cloud.dwavesys.com/leap/'>website</Link>.
+              </Typography>
+              <Button color='geeringup' onClick={handleClickOpen('paper')}>Find Out More</Button>
+            </CardBody>
+          </form>
+        </Card>
+      </GridItem>
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        scroll={scroll}
+        aria-labelledby='scroll-dialog-title'
+        aria-describedby='scroll-dialog-description'
+      >
+        <DialogTitle id='scroll-dialog-title'>Instructions</DialogTitle>
+        <DialogContent dividers={scroll === 'paper'}>
+          <DialogContentText
+            id='scroll-dialog-description'
+            ref={descriptionElementRef}
+            tabIndex={-1}
+          >
+            <Typography align='left' paragraph>
+              Select 'QPU' on the slider, input your API token, and click 'Submit'. You should see a
+              confirmation message at the bottom of the page! You can navigate to other tabs, but if you
+              refresh the page, you'll have to re-submit your API token.
+            </Typography>
+            <Typography align='left' paragraph>
+              Select a cell on the Sudoku grid. Type in your desired digit. After entering the puzzle,
+              press the 'Solve' button below.
+            </Typography>
+            <Typography align='left' paragraph>
+              When you submit a problem to one of D-Wave's solvers, you are put in a queue. The time it
+              takes to solve your Sudoku grid will depend on where you are in that queue.
+            </Typography>
+            <Typography align='left' paragraph>
+              D-Wave's solvers are probabilistic, meaning that each run <i>might</i> result in a different
+              solution. That is why problems are <i>sampled</i> many times, to make sure that the correct
+              solution is identified. Well crafted Sudoku puzzles have <i>one</i> unique solution. In some
+              cases, you can design a Sudoku puzzle such that there may be multiple possible solutions.
+            </Typography>
+            <Typography align='left' paragraph>
+              For instance, what do you think will happen if you submit a blank grid to the solver? Try it
+              out multiple times and see if you get the same answer each time!
+            </Typography>
+            <Typography align='left' paragraph>
+              This application uses a script from D-Wave's gallery of examples
+              linked <Link href='https://github.com/dwave-examples/sudoku'>here</Link>. The Sudoku puzzle is modeled as
+              a <i>constraint satisfaction problem</i>, or CSP. These are problems in mathematics where the
+              solution is the state of a set of objects such that they satisfy certain constraints or
+              limitations. For instance, in Sudoku, there are a number of requirements or constraints for a
+              solution to be correct.
+            </Typography>
+            <Typography align='left' paragraph>
+              Each cell in the Sudoku puzzle must contain <i>one and only one</i> digit
+              <ul>
+                <li><b>No</b> row may have duplicate digits</li>
+                <li><b>No</b> column may have duplicate digits</li>
+                <li><b>No</b> subgrid may have duplicate digits</li>
+              </ul>
+            </Typography>
+            <Typography align='left' paragraph>
+              CSPs allow for finding useful general-purpose algorithms that help tackle many everyday problems.
+              The applications of CSPs range from solving logic puzzles and scheduling nurses on shifts to
+              applications in artifical intelligence and building neural networks.
+            </Typography>
+            <Typography align='left' paragraph>
+              D-Wave's sudoku-solving script uses a hybrid
+              solver, <Link href='https://docs.dwavesys.com/docs/latest/doc_leap_hybrid.html'>Kerberos</Link>. Kerberos breaks
+              problems down into smaller pieces and solves each on a quantum computer, before combining them to
+              provide one final solution.
+            </Typography>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color='geeringup'>
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </GridContainer>
   )
 }
